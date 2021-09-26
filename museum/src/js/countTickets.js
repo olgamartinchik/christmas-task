dd6d24c7e18b08abe46b82d9fb8982103b8deab1
olgamartinchik <sintcerova@mail.ru>
2021-09-26 21:27:42 +0300

    feat:add slider in welcome section

---
+++ museum/src/js/countTickets.js
@@ -0,0 +1,37 @@
+export let countTickets = () => {
+  let count = 0;
+  let inputValueBasic = document.querySelector(".input_number_basic").value;
+  let plusBtnBasic = document.querySelector(".plus_btn_basic");
+  let minusBtnBasic = document.querySelector(".minus_btn_basic");
+  let inputValueSenior = document.querySelector(".input_number_senior").value;
+  let plusBtnSenior = document.querySelector(".plus_btn_senior");
+  let minusBtnSenior = document.querySelector(".minus_btn_senior");
+  plusBtnBasic.addEventListener("click", (e) => {
+    count++;
+    inputValueBasic = count;
+    document.querySelector(".input_number_basic").value = inputValueBasic;
+  });
+  minusBtnBasic.addEventListener("click", (e) => {
+    count--;
+    inputValueBasic = count;
+    document.querySelector(".input_number_basic").value = inputValueBasic;
+    if (count <= 0) {
+      document.querySelector(".input_number_basic").value = 0;
+    }
+  });
+  plusBtnSenior.addEventListener("click", (e) => {
+    count++;
+    inputValueSenior = count;
+    document.querySelector(".input_number_senior").value = inputValueSenior;
+  });
+  minusBtnSenior.addEventListener("click", (e) => {
+    count--;
+    inputValueSenior = count;
+    document.querySelector(".input_number_senior").value = inputValueSenior;
+    if (count <= 0) {
+      document.querySelector(".input_number_senior").value = 0;
+    }
+  });
+};
+
+countTickets();
