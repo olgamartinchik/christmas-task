0bf7dc0529b20dd81c3cade07b2d0b4f09848706
olgamartinchik <sintcerova@mail.ru>
2021-09-30 21:29:45 +0300

    feat:add line-effect for card

--- museum/src/js/fullscreen.js
+++ museum/src/js/fullscreen.js
@@ -1,17 +1,7 @@
 let discoveryBtn = document.querySelector(".welcome_btn");
 let iframeFullScreen = document.getElementById("fullscreen");
 
-// let fullscreen = () => {
-//   if (document.exitFullscreen) {
-//     document.exitFullscreen();
-//     iframeFullScreen.style.display = "none";
-//   }
 discoveryBtn.addEventListener("click", () => {
   iframeFullScreen.style.display = "block";
   iframeFullScreen.requestFullscreen();
-  // if (document.exitFullscreen) {
-  //   document.exitFullscreen();
-  //   iframeFullScreen.style.display = "none";
-  // }
 });
-// };
