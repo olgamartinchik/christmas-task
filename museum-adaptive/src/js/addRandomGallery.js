d30b4bce18a344a2aa9883feaa6ee4b47e763e8c
olgamartinchik <sintcerova@mail.ru>
2021-10-04 21:21:37 +0300

    feat: add popup responsive, add style responsive

--- museum-adaptive/src/js/addRandomGallery.js
+++ museum-adaptive/src/js/addRandomGallery.js
@@ -5,21 +5,21 @@ export let shuffle = (array) => {
 export let addRandomGallery = () => {
   // console.log("hello");
   let dataImagesGallery = [
-    "galery1.jpg",
-    "galery2.jpg",
-    "galery3.jpg",
-    "galery4.jpg",
-    "galery5.jpg",
-    "galery6.jpg",
-    "galery7.jpg",
-    "galery8.jpg",
-    "galery9.jpg",
-    "galery10.jpg",
-    "galery11.jpg",
-    "galery12.jpg",
-    "galery13.jpg",
-    "galery14.jpg",
-    "galery15.jpg",
+    "galery1.jpeg",
+    "galery2.jpeg",
+    "galery3.jpeg",
+    "galery4.jpeg",
+    "galery5.jpeg",
+    "galery6.jpeg",
+    "galery7.jpeg",
+    "galery8.jpeg",
+    "galery9.jpeg",
+    "galery10.jpeg",
+    "galery11.jpeg",
+    "galery12.jpeg",
+    "galery13.jpeg",
+    "galery14.jpeg",
+    "galery15.jpeg",
   ];
 
   shuffle(dataImagesGallery);
