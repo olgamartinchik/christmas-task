e80d62d179b31f6b7aa06ab8b09bfeac6483c1f6
olgamartinchik <sintcerova@mail.ru>
2021-11-13 10:06:58 +0300

    feat: add section artists_quiz

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -6,13 +6,16 @@ const settings = document.querySelector(".setting_page");
 const main_btn = document.querySelector(".main_btn");
 const logo = document.querySelector(".logo");
 const sections = document.querySelectorAll("section");
-const homeBtn = document.querySelector(".home_btn");
+const homeBtns = document.querySelectorAll(".home_btn");
 main_btn.addEventListener("click", (e) => {
   main.classList.add("active");
   settings.classList.remove("active");
 });
 logo.addEventListener("click", toggleMainPage);
-homeBtn.addEventListener("click", toggleMainPage);
+homeBtns.forEach((homeBtn) => {
+  homeBtn.addEventListener("click", toggleMainPage);
+});
+
 function toggleMainPage() {
   sections.forEach((section) => {
     section.classList.add("active");
